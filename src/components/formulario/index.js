cddeb2d3d474c1f9ee5d9bf60252707909c9068f
RAYSON VINICIUS SOUZA <88290555+RaysomSousa@users.noreply.github.com>
2022-08-31 01:00:06 -0300

    Update index.js

--- src/components/formulario/index.js
+++ src/components/formulario/index.js
@@ -18,9 +18,9 @@ const aoSalvar = (evento) =>{
     return(
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
-            <h2 className='titulo-formulario'>Prencha os dados para criar o card do player.</h2>
+            <h2 className='titulo-formulario'>Prencha os dados para criar o card.</h2>
             <CampoTexto obrigatorio={true} label='Nome'  placeholder="Digite seu nome..."/>
-            {/* <CampoTexto  label='Cargo' placeholder="Digite seu cargo..."/> */}
+            <CampoTexto obrigatorio={true} label='Nick' placeholder="Digite seu nick de jogador..."/>
             <CampoTexto  label='Imagem' placeholder="Digite o endereço da imagem..."/>
             <ListaSuspensa label='Time' itens={times}/>
             <Botao>
